f5b8eb338eb2f9fc99d74a7ab197ea87f2d67319
Mikel Azkolain <azkotoki@gmail.com>
2014-01-31 16:02:12 +0100

    Updated package version.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='TaskUtils',
-    version='0.1',
+    version='0.2',
     description='A passive background task manager.',
     author='Mikel Azkolain',
     author_email='azkotoki@gmail.com',
